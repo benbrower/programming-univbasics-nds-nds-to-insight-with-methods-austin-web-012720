a570b398f3ccb7fb4eee04d258a71fcbb6bfccb5
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-24 05:43:33 +0000

    Automatically backed up by Learn

--- spec/nds_extract_spec.rb
+++ spec/nds_extract_spec.rb
@@ -14,7 +14,7 @@ end
 #   it 'which returns a Hash describing director to total' do
 #     expect(directors_totals([{ :name => "Test Director", :movies => []}])).to be_a(Hash)
 #   end
-# 
+#
 #   describe "and correctly totals the directors' totals" do
 #     let(:expected) {
 #       {
@@ -29,39 +29,39 @@ end
 #         "Francis Ford Coppola"=>509719258
 #       }
 #     }
-# 
+#
 #     it "correctly totals 'Stephen Spielberg'" do
 #       expect(directors_totals(directors_database)['Stephen Spielberg']).to eq(expected['Stephen Spielberg'])
 #     end
-# 
+#
 #     it "correctly totals 'Russo Brothers'" do
 #       expect(directors_totals(directors_database)['Russo Brothers']).to eq(expected['Russo Brothers'])
 #     end
-# 
+#
 #     it "correctly totals 'James Cameron'" do
 #       expect(directors_totals(directors_database)['James Cameron']).to eq(expected['James Cameron'])
 #     end
-# 
+#
 #     it "correctly totals 'Spike Lee'" do
 #       expect(directors_totals(directors_database)['Spike Lee']).to eq(expected['Spike Lee'])
 #     end
-# 
+#
 #     it "correctly totals 'Wachowski Siblings'" do
 #       expect(directors_totals(directors_database)['Wachowski Siblings']).to eq(expected['Wachowski Siblings'])
 #     end
-# 
+#
 #     it "correctly totals 'Robert Zemeckis'" do
 #       expect(directors_totals(directors_database)['Robert Zemeckis']).to eq(expected['Robert Zemeckis'])
 #     end
-# 
+#
 #     it "correctly totals 'Quentin Tarantino'" do
 #       expect(directors_totals(directors_database)['Quentin Tarantino']).to eq(expected['Quentin Tarantino'])
 #     end
-# 
+#
 #     it "correctly totals 'Martin Scorsese'" do
 #       expect(directors_totals(directors_database)['Martin Scorsese']).to eq(expected['Martin Scorsese'])
 #     end
-# 
+#
 #     it "correctly totals 'Francis Ford Coppola'" do
 #       expect(directors_totals(directors_database)['Francis Ford Coppola']).to eq(expected['Francis Ford Coppola'])
 #     end
