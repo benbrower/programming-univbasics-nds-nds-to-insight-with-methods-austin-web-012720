0f50bf5ff883f94c2b715bffbdc9b2a40216bc06
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-24 05:50:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,7 @@ def gross_for_director(director_data)
 
   gross_total = 0
   movie_index = 0
-  while movie_index < director_data.size do
+  while movie_index < director_data[movie_index].size do
     title_index = 0
     while title_index < director_data[movie_index].size do
       gross_total += director_data[movie_index][worldwide_gross][title_index]
