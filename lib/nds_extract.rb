86e6a8a88e979007e662eecb00cb322bb6cc4ee5
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-24 05:34:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,11 +2,10 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
 
-pp director_data
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-
+  pp director_data
 end
 
 # Write a method that, given an NDS creates a new Hash
